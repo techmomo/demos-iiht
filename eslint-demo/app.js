3170af490b2538acc309d51959f464b00ba80d65
techmomo <mohsin.khursheed@techjini.com>
2021-04-22 10:37:27 +0530

    Es lint

---
+++ eslint-demo/app.js
@@ -0,0 +1,9 @@
+console.log("hello")
+const num1=10
+console.log(`Num1 is ${num1}`)
+
+const sayHello = ()=>{
+    console.log("Hola!")
+}
+
+sayHello()
---
+++ eslint-demo/server.js
@@ -0,0 +1,10 @@
+let username = "Mohsin"
+const employee ={name:username}
+username="tt"
+console.log(employee)
+
+const getMessage=(name)=>{
+    console.log(`Hello Eslint ${name}`);
+};
+
+getMessage("some message")
