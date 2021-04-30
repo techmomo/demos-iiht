b9fd23659c745915bcc73fd270f01fc30dea6d9f
techmomo <mohsin.khursheed@techjini.com>
2021-04-30 10:30:48 +0530

    Spring Redis

--- amqp-demo/src/main/java/com/mohsinkd786/MessageListener.java
+++ amqp-demo/src/main/java/com/mohsinkd786/MessageListener.java
@@ -3,16 +3,16 @@ package com.mohsinkd786;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class MessageListener {
 
-//    @KafkaListener(topics = "demo-iiht",groupId = "ctsGrp")
-//    public void onMessage(String message){
-//        System.out.println("Kafka Listener - START");
-//        System.out.println(message);
-//        System.out.println("Kafka Listener - END");
-//    }
-//
+    @KafkaListener(topics = "demo-iiht",groupId = "ctsGrp")
+    public void onMessage(String message){
+        System.out.println("Kafka Listener - START");
+        System.out.println(message);
+        System.out.println("Kafka Listener - END");
+    }
+
 //    @KafkaListener(topics = "demo-iiht",groupId = "ctsGrp")
 //    public void onMessage1(String message){
 //        System.out.println("Kafka Listener - START 1");
